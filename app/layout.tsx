ccc5d45a0f0f1caef160314ca5001c3d3517ec49
周树庆 <zhoushq@cread.com>
2024-06-18 19:59:00 +0800

    修改sidebar，刪除部分代碼

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChenChat",
+  description: "Chenxin's ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
